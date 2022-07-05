3d48a833f9a5691d49b31e454b870115b8b751af
bartosz121 <bmagiera121@gmail.com>
2022-07-05 22:12:53 +0200

    Check for forbidden moves, fix imports

--- snake_py/main.py
+++ snake_py/main.py
@@ -2,8 +2,8 @@ import curses
 import operator
 import random
 
-from snake_py import snake
-from snake_py.controller import Controller, ControllerConfig, Direction
+from snake_py import snake, snake_move, snake_config, direction
+from snake_py.controller import Controller, ControllerConfig
 from snake_py.drawer import Drawer
 from snake_py.game_obj import GameObj
 from snake_py.position import Position
@@ -11,7 +11,7 @@ from snake_py.position import Position
 
 class CursesDrawer(Drawer):
     def __init__(
-        self, config: snake.SnakeConfig, stdscr: "curses._CursesWindow"
+        self, config: snake_config.SnakeConfig, stdscr: "curses._CursesWindow"
     ) -> None:
         super().__init__(config)
         self.stdscr = stdscr
@@ -28,25 +28,23 @@ class CursesController(Controller):
     def __init__(self, config: ControllerConfig) -> None:
         super().__init__(config)
 
-    def get_pos_after_turn(
-        self, direction: Direction, current_pos: Position
-    ) -> Position:
-        if direction == Direction.UP:
+    def get_pos_after_turn(self, move: snake_move.SnakeMove) -> Position:
+        if move.direction == direction.Direction.UP:
             op = operator.sub
             target = "y"
-        elif direction == Direction.LEFT:
+        elif move.direction == direction.Direction.LEFT:
             op = operator.sub
             target = "x"
-        elif direction == Direction.DOWN:
+        elif move.direction == direction.Direction.DOWN:
             op = operator.add
             target = "y"
-        elif direction == Direction.RIGHT:
+        elif move.direction == direction.Direction.RIGHT:
             op = operator.add
             target = "x"
         else:
-            raise AssertionError(f"Unknown direction: {direction}")
+            raise AssertionError(f"Unknown direction: {move.direction}")
 
-        new_pos = Position(*current_pos)
+        new_pos = Position(*move.snake_pos)
         old_target = getattr(new_pos, target)
         setattr(new_pos, target, op(old_target, 1))
 
@@ -86,7 +84,7 @@ def c_main(stdscr: "curses._CursesWindow") -> int:
     # center_y = curses.LINES // 2
     # center_x = curses.COLS // 2
 
-    snake_config = snake.SnakeConfig(snake_char="O", apple_char="$")
+    snake_conf = snake_config.SnakeConfig(snake_char="O", apple_char="$")
     controller_config = ControllerConfig(
         curses.KEY_UP,
         curses.KEY_RIGHT,
@@ -97,12 +95,14 @@ def c_main(stdscr: "curses._CursesWindow") -> int:
     curses_napms = 110
     curses_napms_step_after_apple_eaten = -2
 
-    drawer = CursesDrawer(snake_config, stdscr)
+    drawer = CursesDrawer(snake_conf, stdscr)
+
+    drct = direction.Direction.RIGHT
     controller = CursesController(controller_config)
+
     snake_ = snake.Snake(start_pos)
     apple = GameObj(get_random_pos())
 
-    direction = Direction.RIGHT
     while True:
         curses.napms(curses_napms)
         stdscr.erase()
@@ -116,8 +116,11 @@ def c_main(stdscr: "curses._CursesWindow") -> int:
             apple.pos = get_random_pos()
             curses_napms += curses_napms_step_after_apple_eaten
 
-        new_pos = controller.get_pos_after_turn(direction, snake_.get_pos())
+        new_pos = controller.get_pos_after_turn(
+            snake_move.SnakeMove(drct, snake_.get_pos())
+        )
         new_pos = validate_position(new_pos)
+
         snake_.change_pos(new_pos)
 
         try:
@@ -128,15 +131,18 @@ def c_main(stdscr: "curses._CursesWindow") -> int:
             if ch == 113:  # q
                 break
             if ch == curses.KEY_UP:
-                direction = Direction.UP
+                new_drct = direction.Direction.UP
             elif ch == curses.KEY_RIGHT:
-                direction = Direction.RIGHT
+                new_drct = direction.Direction.RIGHT
             elif ch == curses.KEY_DOWN:
-                direction = Direction.DOWN
+                new_drct = direction.Direction.DOWN
             elif ch == curses.KEY_LEFT:
-                direction = Direction.LEFT
+                new_drct = direction.Direction.LEFT
             else:
                 continue
+
+            if not controller.is_move_forbidden(drct, new_drct, len(snake_.body)):
+                drct = new_drct
 
     return 0
 
